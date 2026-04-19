7ed7c6d958449ce3e29f102e59c48b135a22f424
ImDarkly <svit.darkly@gmail.com>
2026-04-19 16:22:06 +0300

    refactor(ui): adjust Game layout flex growth behavior

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -10,7 +10,7 @@ export default function Game() {
 
   return (
     <RoomLayout roomId={roomId}>
-      <div className="@container-[size] flex h-full min-h-0 w-full items-center justify-center">
+      <div className="@container-[size] flex min-h-0 w-full flex-1 items-center justify-center">
         <Board />
       </div>
       <Rack />
